491eda11807ce929b24b262d053b1c360737b1c8
James Young <jmeyoung@gmail.com>
2016-02-25 17:12:43 -0500

    wip

--- generators/app/templates/server.js
+++ generators/app/templates/server.js
@@ -1,11 +1,22 @@
 'use strict';
 
+/*
+ * Example application that uses cnn-hapi as a dependency to provide a basic
+ * Hapi server with built in features that this example doesn't need to care
+ * about.
+ *
+ * See the comments inline for changes that would be typical in an external app
+ */
+
 const hapi = require('cnn-hapi');
 
-let app = hapi({
+let app = module.exports = hapi({
     directory: __dirname,
     port: process.env.PORT,
-    withSwagger: true
+    withSwagger: true,
+    withNavigation: false,
+    metrics: {provider: require('cnn-metrics'), options: {flushEvery: 1000 * 20}},
+    layoutsDir: `${__dirname}/views/`
 });
 
 app.route({
